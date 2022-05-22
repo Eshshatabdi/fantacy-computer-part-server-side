239eece1ec62d598f298d0394ad09e9de37080a6
Esh shatabdi <eshshatabdi@gmail.com>
2022-05-21 22:12:04 -0400

    basic setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Running Manufacture server')
+})
+
+app.listen(port, () => {
+    console.log('server is running', port)
+})
